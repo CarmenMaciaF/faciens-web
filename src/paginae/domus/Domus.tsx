10d1218a49b54974b3a0156f472abd1800d1a77a
Carmen Macia <carmacia03@gmail.com>
2024-10-27 19:55:15 +0100

    RSH FACIENS-WEB 06

--- src/paginae/domus/Domus.tsx
+++ src/paginae/domus/Domus.tsx
@@ -1,28 +1,42 @@
-import { useState } from "react";
-import estilus from './estilus.module.css';
 import { NavLink } from "react-router-dom";
 
 export const Domus = () => {
 
-  const [numerus, setnumerus] = useState<number>(0);
-
-  const {titulo, tonitruum, botones} = estilus;
+ 
 
   return (
     <>
-    <h1 id="title" className={titulo}> {numerus%2 === 0 ? 'Diseño web' : 'Apps interactivas'} </h1>
+    <div className="text-white font-sans pt-36 w-screen h-screen">
+      <div className="flex flex-col justify-center items-center text-center gap-12">
 
-    <img onClick={() => setnumerus(numerus +1)} className={tonitruum} src="./tonitrui.png" alt="" />
+        <img src="./imagenes/tonitrui.png" className="absolute opacity-20" alt=""/>
+
+        <h1 className="text-5xl md:text-7xl lg:text-8xl font-bold pb-5 md:pb-20 z-10">
+          <span className="text-[var(--color-caelum)]">Diseño </span>Web y 
+          <br/>
+          Apps <span className="text-[var(--color-caelum)]">Interactivas</span> 
+        </h1>
+
+        <p className="text-lg md:text-2xl">Aprendiendo a crear aplicaciones web</p>
+
+        <div className="flex gap-4 z-10"> 
+          <NavLink to={''} className="button">Usuarios</NavLink>
+
+          <NavLink to={'/batman'} className="button">Batman</NavLink>
+
+          <NavLink to={''} className="button">Simpsons</NavLink>
+          
+          <a className="button bg-gray-700" href="https://www.upv.es/">
+              Videos clase
+           </a>
+        </div>
+
+      </div>
+
+    </div>
+     
     
-    <div className={botones}> 
-	    <NavLink to={''}>Usuarios</NavLink>
-	    <NavLink to={'/batman'}>Batman</NavLink>
-	    <NavLink to={''}>Simpsons</NavLink>
-	    <a href="https://www.upv.es/">
-		    Videos clase
-	    </a>
-</div>
-
+    
     
     </>
   )
